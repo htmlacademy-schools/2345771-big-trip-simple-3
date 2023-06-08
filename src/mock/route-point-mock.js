403c84bb87a65ae02e4c811799c94577e0c08da9
Maxim Kirillov <maks_kirillov_2003@mail.ru>
2023-06-08 14:20:31 +0300

    last commit

--- src/mock/route-point-mock.js
+++ src/mock/route-point-mock.js
@@ -1,69 +1,4 @@
-import {getRandomInteger} from '../utils.js';
 import {nanoid} from 'nanoid';
-
-const generateDate = () => {
-  const dates = [
-    ['2019-07-10T22:55:56.845Z', '2019-07-11T11:22:13.375Z'],
-    ['2019-06-23T23:51:56.845Z', '2019-06-24T10:22:43.375Z'],
-    ['2019-09-12T21:45:56.845Z', '2019-09-13T09:22:15.375Z'],
-    ['2019-08-15T20:36:56.845Z', '2019-08-16T08:22:23.375Z'],
-    ['2019-11-16T23:28:56.845Z', '2019-11-17T07:22:19.375Z'],
-  ];
-
-  const randomIndex = getRandomInteger(0, dates.length - 1);
-
-  return dates[randomIndex];
-};
-
-const generateDestination = () => {
-  const destinations = ['London', 'Paris', 'Berlin', 'Moscow', 'Rome', 'Instanbul', 'Madrid', 'Budapest'];
-
-  const randomIndex = getRandomInteger(0, destinations.length - 1);
-
-  return destinations[randomIndex];
-};
-
-const generateOffers = () => {
-  const offers = [
-    {
-      id: 1,
-      title: 'Add luggage',
-      price: 50
-    },
-    {
-      id: 2,
-      title: 'Switch to comfort class',
-      price: 80
-    },
-    {
-      id: 3,
-      title: 'Add meal',
-      price: 15
-    },
-    {
-      id: 4,
-      title: 'Choose seats',
-      price: 5
-    },
-    {
-      id: 5,
-      title: 'Travel by train',
-      price: 40
-    }
-  ];
-
-  const randomIndex = getRandomInteger(0, offers.length - 2);
-
-  return [offers[randomIndex],offers[randomIndex + 1]];
-};
-
-const generateType = () => {
-  const types = ['taxi', 'bus', 'train', 'ship', 'drive', 'flight', 'check-in', 'sightseeing', 'restaurant'];
-
-  const randomIndex = getRandomInteger(0, types.length - 1);
-
-  return types[randomIndex];
-};
 
 export const comparePointsByPriceHihgLow = (a, b) => {
   if ( a.basePrice < b.basePrice ){
@@ -100,17 +35,14 @@ export const filterToCurrentDate = (a) => {
   return 0;
 };
 
-export const getRoutePoint = () => {
-  const date = generateDate();
-  return ({
-    basePrice: getRandomInteger(1100, 1900),
-    dateFrom: date[0],
-    dateTo: date[1],
-    destination: generateDestination(),
-    id: nanoid(),
-    offers: generateOffers(),
-    type: generateType()
-  });
-};
+export const getRoutePoint = () => ({
+  basePrice: 500,
+  dateFrom: new Date().toJSON(),
+  dateTo: new Date().toJSON(),
+  destination: 5,
+  id: nanoid(),
+  offers: [],
+  type: 'taxi'
+});
 
 
